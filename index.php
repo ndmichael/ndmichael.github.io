df3539340a61519eb8238f1dad645408e9693eac
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-08-29 22:28:30 +0100

    Took the project section to the top below the landing page

--- index.php
+++ index.php
@@ -55,6 +55,273 @@
 		</div> <!-- end of the container -->
 	</section>
 	
+	
+	
+	
+	<section id="projects">
+		<div class="container">
+			<h1>Projects</h1>
+			<ul class="nav nav-pills my-3" id="myTab" role="tablist">
+			  <li class="nav-item">
+				<a class="nav-link active" id="home-tab" data-toggle="tab" href="#all" role="tab" aria-controls="home" aria-selected="true">All</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Web</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Django</a>
+			  </li>
+				<li class="nav-item">
+				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">ReactJS</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Algorithm</a>
+			  </li>
+			</ul>
+			
+			
+			<div class="tab-content" id="myTabContent">
+			  <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="home-tab">
+				<div class="row">
+					  <div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+						  <img class="card-img-top img-fluid" src="images/kiki_port.jpg" alt="Card image cap">
+						  <div class="card-header">
+							<h5 >E-Commerce </h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An E-Commerce website built on Django for medium scale business, Utilizing django middewares.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- E commerce card  -->	 
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+						  <img class="card-img-top img-fluid" src="images/CMForm2.png" alt="Card image cap">
+						  <div class="card-header">
+							<h5 >Student Class Form </h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">Class form built on React using formik reusable component, validated with Yup and styled with ChakaUI.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Class Management Form  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/number_pyramid.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Numeric Pyramid Pattern</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An algorithm that prints numbers in reverse and direct chronological pattern creating a pyramid.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!--Number Pyramid Card -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/LottoGame.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Lotto Game</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">Number Lotto game built with vanilla Javascript. It incremen tuser values depending on how many correct guess.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Lotto Game Card  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Recipe App</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">Differe3nt from your regular app, supports user blogging, authentication and allows users to upload their own recipes.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Recipe app card  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Beautiful Heart Pattern</h5>
+						  </div>
+						  <div class="card-body h-100">
+							<p class="card-text">A simple application that print different colorful heart pattern depending on user selection between 1 - 7.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Beautiful Heart Pattern Card  -->
+					</div>
+					
+					<!-- cards row 3 -->
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Number Converter</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">A number comverter app built on react that converts number depending on the selected choice. works for binary, hexadecimal, and octal.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Class Management Form  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>File Scheduling</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An algorithm to handles files in C. Write and read files, return the number of lines, characters and words in a file.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- File Scheduling Card  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/portfolio_port2.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Portfolio Website</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">Afully responsive portfolio that showcase my work and details about my software programming skills.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Github Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Portfolio Card  -->
+					</div>
+					
+					
+					
+					
+					
+					
+					
+					
+					
+						  
+					
+				</div>
+			  	
+			  </div> <!-- end of all tab -->
+				
+				
+				
+				
+				
+				
+			  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+			  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+			</div>
+			
+				
+		</div>
+	</section>
+		
+	
+	
+	
+	
+	
 <!-- section 3 row 3 and row 4 -->
 	<section id="about">
 		<div class="container">
@@ -217,263 +484,7 @@
 		</div>
 	</section>
 		
-	<section id="projects">
-		<div class="container">
-			<h1>Projects</h1>
-			<ul class="nav nav-pills my-3" id="myTab" role="tablist">
-			  <li class="nav-item">
-				<a class="nav-link active" id="home-tab" data-toggle="tab" href="#all" role="tab" aria-controls="home" aria-selected="true">All</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Web</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Django</a>
-			  </li>
-				<li class="nav-item">
-				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">ReactJS</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Algorithm</a>
-			  </li>
-			</ul>
-			
-			
-			<div class="tab-content" id="myTabContent">
-			  <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="home-tab">
-				<div class="row">
-					  <div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-						  <img class="card-img-top img-fluid" src="images/kiki_port.jpg" alt="Card image cap">
-						  <div class="card-header">
-							<h5 >E-Commerce </h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- E commerce card  -->	 
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-						  <img class="card-img-top img-fluid" src="images/CMForm2.png" alt="Card image cap">
-						  <div class="card-header">
-							<h5 >Student Class Form </h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Class Management Form  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/number_pyramid.png" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Numeric Pyramid Pattern</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!--Number Pyramid Card -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/LottoGame.png" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Lotto Game</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Lotto Game Card  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Recipe App</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Class Management Form  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Beautiful Heart Pattern</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Beautiful Heart Pattern Card  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Number Converter</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Class Management Form  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>File Scheduling</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Beautiful Heart Pattern Card  -->
-					</div>
-					
-					<div class="col col-12 col-md-6 col-lg-4 ">
-						<div class="card">
-							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
-						  
-						  <div class="card-header">
-							<h5>Portfolio Website</h5>
-						  </div>
-						  <div class="card-body">
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  </div>
-						  <div class="card-footer">
-							<div class="d-flex flex-row justify-content-between">
-								<span>
-									<button class="btn btn-warning">Live Preview</button>
-								</span>
-								<span>
-									<button class="btn btn-outline-light">Github Code</button>
-								</span>
-							</div>
-						  </div>
-						</div> <!-- Portfolio Card  -->
-					</div>
-					
-					
-					
-					
-					
-					
-					
-					
-					
-						  
-					
-				</div>
-			  	
-			  </div> <!-- end of all tab -->
-				
-				
-				
-				
-				
-				
-			  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-			  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
-			</div>
-			
-				
-		</div>
-	</section>
-		
+	
 	
 	
 <!-- referencing the codeja-vu footer -->
