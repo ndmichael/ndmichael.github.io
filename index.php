bdaee577f1bfcacd1356e38e56406c893c67e0bb
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-08-31 20:23:27 +0100

    Added the react and algorithm tabs

--- index.php
+++ index.php
@@ -82,13 +82,13 @@
 				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#web" role="tab" aria-controls="profile" aria-selected="false">Web</a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Django</a>
+				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#django" role="tab" aria-controls="contact" aria-selected="false">Django</a>
 			  </li>
 				<li class="nav-item">
-				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">ReactJS</a>
+				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#react" role="tab" aria-controls="profile" aria-selected="false">ReactJS</a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Algorithm</a>
+				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#algorithm" role="tab" aria-controls="contact" aria-selected="false">Algorithm</a>
 			  </li>
 			</ul>
 			
@@ -457,8 +457,199 @@
 			  </div>
 				
 				
-			  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
-			</div>
+			  <div class="tab-pane fade" id="django" role="tabpanel" aria-labelledby="contact-tab">
+			  	<div class="row">
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+						  <img class="card-img-top img-fluid" src="images/kiki_port.jpg" alt="Card image cap">
+						  <div class="card-header">
+							<h5 >E-Commerce </h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An E-Commerce website built on Django for medium scale business, Utilizing django middewares.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- E commerce card  -->	 
+					</div>
+				  
+				    <div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Recipe App</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">Differe3nt from your regular app, supports user blogging, authentication and allows users to upload their own recipes.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Recipe app card  -->
+					</div>  
+				</div>
+			  </div> <!-- end of django tab section -->
+				
+			<div class="tab-pane fade" id="react" role="tabpanel" aria-labelledby="contact-tab">
+				<div class="row">
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/Untitled design.jpg" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Number Converter</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">A number converter app built on react that converts number depending on the selected choice. works for binary, hexadecimal, octal, and decimal.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Number Converter card  -->
+					</div>
+				
+					<div class="col col-12 col-md-6 col-lg-4 ">
+							<div class="card">
+							  <img class="card-img-top img-fluid" src="images/CMForm2.png" alt="Card image cap">
+							  <div class="card-header">
+								<h5 >Student Class Form </h5>
+							  </div>
+							  <div class="card-body">
+								<p class="card-text">Class form built on React using formik reusable component, validated with Yup and styled with ChakaUI.</p>
+							  </div>
+							  <div class="card-footer">
+								<div class="d-flex flex-row justify-content-between">
+									<span>
+										<button class="btn btn-outline-warning">Live Preview</button>
+									</span>
+									<span>
+										<button class="btn btn-outline-light">Source Code</button>
+									</span>
+								</div>
+							  </div>
+							</div> <!-- Class Management Form card  -->
+						</div>
+				
+						<div class="col col-12 col-md-6 col-lg-4 ">
+							<div class="card">
+							  <img class="card-img-top img-fluid" src="images/LottoGame.png" alt="Card image cap">
+
+							  <div class="card-header">
+								<h5>Lotto Game</h5>
+							  </div>
+							  <div class="card-body">
+								<p class="card-text">Number Lotto game built with vanilla Javascript. It incremen t/ decrement user money depending on how many correct guess.</p>
+							  </div>
+							  <div class="card-footer">
+								<div class="d-flex flex-row justify-content-between">
+									<span>
+										<button class="btn btn-outline-warning">Live Preview</button>
+									</span>
+									<span>
+										<button class="btn btn-outline-light">Source Code</button>
+									</span>
+								</div>
+							  </div>
+						</div> <!-- Lotto Game Card  -->
+					</div>
+				</div>
+			</div> <!-- end of react tab section -->
+				
+			<div class="tab-pane fade" id="algorithm" role="tabpanel" aria-labelledby="contact-tab">
+				<div class="row">
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/number_pyramid.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Numeric Pyramid Pattern</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An algorithm that prints numbers in reverse and direct chronological pattern creating a pyramid.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+<!--									<button class="btn btn-outline-light">Source Code</button>-->
+								<span class="btn btn-outline-light">Source Code</span>
+								</span>
+							</div>
+						  </div>
+						</div> <!--Number Pyramid Card -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>Beautiful Heart Pattern</h5>
+						  </div>
+						  <div class="card-body h-100">
+							<p class="card-text">A simple application that print different colorful heart pattern depending on user selection between 1 - 7.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- Beautiful Heart Pattern Card  -->
+					</div>
+					
+					<div class="col col-12 col-md-6 col-lg-4 ">
+						<div class="card">
+							<img class="card-img-top img-fluid" src="images/HeartPattern.png" alt="Card image cap">
+						  
+						  <div class="card-header">
+							<h5>File Scheduling</h5>
+						  </div>
+						  <div class="card-body">
+							<p class="card-text">An algorithm to handles files in C. Write and read files, return the number of lines, characters and words in a file.</p>
+						  </div>
+						  <div class="card-footer">
+							<div class="d-flex flex-row justify-content-between">
+								<span>
+									<button class="btn btn-outline-warning">Live Preview</button>
+								</span>
+								<span>
+									<button class="btn btn-outline-light">Source Code</button>
+								</span>
+							</div>
+						  </div>
+						</div> <!-- File Scheduling Card  -->
+					</div>
+				</div> <!-- end of algorithm tab section -->
 			
 				
 		</div>
